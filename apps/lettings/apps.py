b27b55259369217214546940290442e8a15819be
Vincent <>
2023-10-19 17:20:59 +0200

    docker + CircleCi

--- apps/lettings/apps.py
+++ apps/lettings/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
 
 
-# class OCLettingsSiteConfig(AppConfig):
-#     name = 'oc_lettings_site'
-
-
 class LettingsConfig(AppConfig):
     name = 'apps.lettings'
 
--- apps/profiles/apps.py
+++ apps/profiles/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
 
 
-# class OCLettingsSiteConfig(AppConfig):
-#     name = 'oc_lettings_site'
-
-
 class ProfilesConfig(AppConfig):
     name = 'apps.profiles'
