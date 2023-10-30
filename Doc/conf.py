a8a3d5ca923cbf1de5f15e764499e6a40e236bef
Vincent <>
2023-10-30 20:19:35 +0100

    rtd conf

---
+++ Doc/conf.py
@@ -0,0 +1,11 @@
+project = u"OCLettinbgs"
+author = u"Vincent LANFRANCHI"
+release = u"1"
+copyright = u"Vincent LANFRANCHI, 2023"
+
+templates_path = ['_templates']
+language = "fr"
+exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
+pygments_style = 'sphinx'
+default_html_theme = 'sphinx_rtd_theme'
+version = u''
